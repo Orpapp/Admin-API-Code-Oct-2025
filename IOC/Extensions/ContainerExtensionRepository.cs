1f8f871d46c8d01e7996b4584b5970db634698dd
Orp <163973753+Orpapp@users.noreply.github.com>
2025-10-24 15:58:45 +0530

    Add files via upload

---
+++ IOC/Extensions/ContainerExtensionRepository.cs
@@ -0,0 +1,38 @@
+﻿using Data.Factory;
+using Data.IFactory;
+using Data.IRepository;
+using Data.Repository;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IOC.Extensions
+{
+    public static class ContainerExtensionRepository
+    {
+        public static void RegisterRepositories(this IServiceCollection services, IConfiguration configuration)
+        {
+            var dbConnectionString = configuration.GetConnectionString("OrpDbConnection");
+            if (string.IsNullOrEmpty(dbConnectionString)) throw new Exception("Empty Database connection string");
+            services.AddTransient(typeof(IRepositoryConfiguration), ctx => new RepositoryConfiguration(dbConnectionString));
+            services.AddTransient<IDbConnectionFactory, DbConnectionFactory>();
+            services.AddTransient<IAccountRepositry, AccountRepository>();
+            services.AddTransient<ITaskRepository, TaskRepository>();
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IProfileRepository, ProfileRepository>();
+            services.AddTransient<IHangFireRepository, HangFireRepository>();
+            services.AddTransient<IFriendRepository, FriendRepository>();
+            services.AddTransient<IRewardRepository, RewardRepository>();
+            services.AddTransient<IGroupRepository, GroupRepository>();
+            services.AddTransient<IPostRepository, PostRepository>();
+            services.AddTransient<ILevelRepository, LevelRepository>();
+            services.AddTransient<IProgressRepository, ProgressRepository>();
+            services.AddTransient<ILevelVoucherRepository, LevelVoucherRepository>();
+            services.AddTransient<IShopRepository, ShopRepository>();
+            services.AddTransient<ISubscriptionRepository, SubscriptionRepository>();
+            services.AddTransient<ISubscriptionProductRepository, SubscriptionProductRepository>();
+            services.AddTransient<IHelpAndTipsRepository, HelpAndTipsRepository>();
+            services.AddTransient<ISendTasksRepository, SendTasksRepository>();
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        }
+    }
+}
---
+++ IOC/Extensions/ContainerExtensionService.cs
@@ -0,0 +1,34 @@
+﻿using Business.IServices;
+using Business.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IOC.Extensions
+{
+    public static class ContainerExtensionService
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.RegisterRepositories(configuration);
+            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<IProfileService, ProfileService>();
+            services.AddScoped<IManageService, ManageService>();
+            services.AddScoped<ICommonService, CommonService>();
+            services.AddScoped<ITaskService, TaskService>();
+            services.AddScoped<ICategoryServices, CategoryService>();
+            services.AddScoped<IHangFireService, HangFireService>();
+            services.AddScoped<IFriendService, FriendService>();
+            services.AddScoped<IRewardService, RewardService>();
+            services.AddScoped<IPostService, PostService>();
+            services.AddScoped<IGroupService, GroupService>();
+            services.AddScoped<ILevelService, LevelService>();
+            services.AddScoped<IProgressService, ProgressService>();
+            services.AddScoped<ILevelVoucherService, LevelVoucherService>();
+            services.AddScoped<IShopService, ShopService>();
+            services.AddScoped<ISubscriptionService, SubscriptionService>();
+            services.AddScoped<IHelpAndTipsService, HelpAndTipsService>();
+            services.AddScoped<ISendTasksService, SendTasksService>();
+
+        }
+    }
+}
